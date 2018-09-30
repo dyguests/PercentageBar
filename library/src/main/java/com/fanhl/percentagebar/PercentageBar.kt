fef38467fe0574f897a7048cdb1b5a030d5871c5
fanhl <fanhonglin@klicen.com>
2018-09-30 14:21:18 +0800

    bar default

--- library/src/main/java/com/fanhl/percentagebar/PercentageBar.kt
+++ library/src/main/java/com/fanhl/percentagebar/PercentageBar.kt
@@ -9,6 +9,7 @@ import android.support.v4.content.ContextCompat
 import android.text.TextPaint
 import android.util.AttributeSet
 import android.view.View
+import kotlin.math.roundToInt
 
 /**
  * 一个百分比条
@@ -35,9 +36,23 @@ class PercentageBar @JvmOverloads constructor(
         }
 
     private var barHeight: Float = 0f
+        set(value) {
+            if (field == value) {
+                return
+            }
+            field = value
+            requestLayout()
+        }
 
     /** 提示文字与百分比条的间距 */
     private var textPadding: Float = 0f
+        set(value) {
+            if (field == value) {
+                return
+            }
+            field = value
+            requestLayout()
+        }
 
     /** 文字垂直居中的偏移值 */
     private var textCenterYOffset: Float = 0f
@@ -55,7 +70,6 @@ class PercentageBar @JvmOverloads constructor(
         }
 
     init {
-
         val a = context.obtainStyledAttributes(attrs, R.styleable.PercentageBar, defStyleAttr, R.style.Widget_Percentage_Bar)
 
         foregroundDrawable = a.getDrawable(R.styleable.PercentageBar_foregroundDrawable) ?: ContextCompat.getDrawable(context, R.drawable.img_percentage_bar_foreground_default)
@@ -63,6 +77,7 @@ class PercentageBar @JvmOverloads constructor(
         barHeight = a.getDimension(R.styleable.PercentageBar_barHeight, context.resources.getDimension(R.dimen.percentage_bar_height_default))
 
         val textSize = a.getDimension(R.styleable.PercentageBar_textSize, context.resources.getDimension(R.dimen.percentage_text_size_default))
+        val textColor = a.getColor(R.styleable.PercentageBar_textColor, ContextCompat.getColor(context, R.color.percentage_text_color_default))
 
         textPadding = a.getDimension(R.styleable.PercentageBar_textPadding, context.resources.getDimension(R.dimen.percentage_text_padidng_default))
 
@@ -70,16 +85,53 @@ class PercentageBar @JvmOverloads constructor(
 
         a.recycle()
 
-
-        textPaint.textSize = textSize
+        textPaint.apply {
+            this.textSize = textSize
+            this.color = textColor
+        }
     }
 
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
-        super.onMeasure(widthMeasureSpec, heightMeasureSpec)
+//        super.onMeasure(widthMeasureSpec, heightMeasureSpec)
 
         val textHint = createTextHint()
         textPaint.getTextBounds(textHint, 0, textHint.length, textBounds)
         textCenterYOffset = -((textPaint.descent() + textPaint.ascent()) / 2)
+
+        val desiredWidth = textBounds.width()
+        val desiredHeight = maxOf(textBounds.height(), barHeight.roundToInt())
+
+        val widthMode = View.MeasureSpec.getMode(widthMeasureSpec)
+        val widthSize = View.MeasureSpec.getSize(widthMeasureSpec)
+        val heightMode = View.MeasureSpec.getMode(heightMeasureSpec)
+        val heightSize = View.MeasureSpec.getSize(heightMeasureSpec)
+
+        //Measure Width
+        val width = if (widthMode == View.MeasureSpec.EXACTLY) {
+            //Must be this size
+            widthSize
+        } else if (widthMode == View.MeasureSpec.AT_MOST) {
+            //Can't be bigger than...
+            Math.min(desiredWidth, widthSize)
+        } else {
+            //Be whatever you want
+            textBounds.width()
+        }
+
+        //Measure Height
+        val height = if (heightMode == View.MeasureSpec.EXACTLY) {
+            //Must be this size
+            heightSize
+        } else if (heightMode == View.MeasureSpec.AT_MOST) {
+            //Can't be bigger than...
+            Math.min(desiredHeight, heightSize)
+        } else {
+            //Be whatever you want
+            textBounds.height()
+        }
+
+        //MUST CALL THIS
+        setMeasuredDimension(width, height)
     }
 
     override fun onDraw(canvas: Canvas?) {
@@ -104,9 +156,9 @@ class PercentageBar @JvmOverloads constructor(
         foregroundDrawable?.apply {
             setBounds(
                 0,
-                (validHeight / 2f - barHeight / 2f).toInt(),
-                barWidthPercent.toInt(),
-                (validHeight / 2f + barHeight / 2f).toInt()
+                (validHeight / 2f - barHeight / 2f).roundToInt(),
+                barWidthPercent.roundToInt(),
+                (validHeight / 2f + barHeight / 2f).roundToInt()
             )
             draw(canvas)
         }
