ed219980149840581b30fc7b63de701a07598f5e
fanhl <fanhonglin@klicen.com>
2018-09-30 11:41:59 +0800

    bar

--- README.md
+++ README.md
@@ -5,3 +5,49 @@
 ## 注
 
 因项目时间紧，很多配置没写
+
+## Import
+
+
+
+## Usage
+
+in xml:
+
+    <com.fanhl.percentagebar.PercentageBar
+        android:layout_width="match_parent"
+        android:layout_height="30dp"
+        android:layout_marginStart="16dp"
+        android:layout_marginTop="16dp"
+        android:layout_marginEnd="16dp"
+        android:padding="4dp"
+        app:layout_constraintEnd_toEndOf="parent"
+        app:layout_constraintStart_toStartOf="parent"
+        app:layout_constraintTop_toTopOf="parent"
+        app:percentage=".43"/>
+
+styleable:
+
+    <declare-styleable name="PercentageBar">
+        <attr name="foregroundDrawable" format="reference"/>
+        <attr name="barHeight" format="dimension"/>
+        <attr name="textSize" format="dimension"/>
+        <attr name="textPadding" format="dimension"/>
+        <attr name="percentage" format="float"/>
+    </declare-styleable>
+
+## License
+
+Copyright 2018 fanhl
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+   http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
--- library/src/main/java/com/fanhl/percentagebar/PercentageBar.kt
+++ library/src/main/java/com/fanhl/percentagebar/PercentageBar.kt
@@ -3,6 +3,7 @@ package com.fanhl.percentagebar
 import android.content.Context
 import android.graphics.Canvas
 import android.graphics.Color
+import android.graphics.Rect
 import android.graphics.drawable.Drawable
 import android.support.v4.content.ContextCompat
 import android.text.TextPaint
@@ -25,8 +26,33 @@ class PercentageBar @JvmOverloads constructor(
     }
 
     private var foregroundDrawable: Drawable? = null
+        set(value) {
+            if (field == value) {
+                return
+            }
+            field = value
+            requestLayout()
+        }
+
+    private var barHeight: Float = 0f
+
+    /** 提示文字与百分比条的间距 */
+    private var textPadding: Float = 0f
+
+    /** 文字垂直居中的偏移值 */
+    private var textCenterYOffset: Float = 0f
+
+    /** 存放尺寸 */
+    private var textBounds = Rect()
 
     var percentage: Float = 0f
+        set(value) {
+            if (field == value) {
+                return
+            }
+            field = value
+            requestLayout()
+        }
 
     init {
 
@@ -34,13 +60,26 @@ class PercentageBar @JvmOverloads constructor(
 
         foregroundDrawable = a.getDrawable(R.styleable.PercentageBar_foregroundDrawable) ?: ContextCompat.getDrawable(context, R.drawable.img_percentage_bar_foreground_default)
 
-        percentage = a.getFloat(R.styleable.PercentageBar_percentage, .5f)
+        barHeight = a.getDimension(R.styleable.PercentageBar_barHeight, context.resources.getDimension(R.dimen.percentage_bar_height_default))
+
         val textSize = a.getDimension(R.styleable.PercentageBar_textSize, context.resources.getDimension(R.dimen.percentage_text_size_default))
+
+        textPadding = a.getDimension(R.styleable.PercentageBar_textPadding, context.resources.getDimension(R.dimen.percentage_text_padidng_default))
+
+        percentage = a.getFloat(R.styleable.PercentageBar_percentage, .5f)
 
         a.recycle()
 
 
         textPaint.textSize = textSize
+    }
+
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec)
+
+        val textHint = createTextHint()
+        textPaint.getTextBounds(textHint, 0, textHint.length, textBounds)
+        textCenterYOffset = -((textPaint.descent() + textPaint.ascent()) / 2)
     }
 
     override fun onDraw(canvas: Canvas?) {
@@ -59,17 +98,19 @@ class PercentageBar @JvmOverloads constructor(
     }
 
     private fun drawValid(canvas: Canvas, validWidth: Int, validHeight: Int) {
-        canvas.drawColor(Color.BLACK)
+        val barWidthTotal = validWidth - textBounds.width() - textPadding
+        val barWidthPercent = barWidthTotal * percentage
+
         foregroundDrawable?.apply {
             setBounds(
                 0,
-                0,
-                (validWidth * percentage).toInt(),
-                validHeight
+                (validHeight / 2f - barHeight / 2f).toInt(),
+                barWidthPercent.toInt(),
+                (validHeight / 2f + barHeight / 2f).toInt()
             )
             draw(canvas)
         }
-        canvas.drawText(createTextHint(), (validWidth * percentage), validHeight / 2f, textPaint)
+        canvas.drawText(createTextHint(), barWidthPercent + textPadding, validHeight / 2f + textCenterYOffset, textPaint)
     }
 
     private fun createTextHint() = NumberUtil.perc(percentage)
