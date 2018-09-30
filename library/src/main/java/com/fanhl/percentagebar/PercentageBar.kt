1448d73587547641d104c34ea38805c2c4ab3cbc
fanhl <fanhonglin@klicen.com>
2018-09-30 10:54:03 +0800

    percent bar

--- library/src/main/java/com/fanhl/percentagebar/PercentageBar.kt
+++ library/src/main/java/com/fanhl/percentagebar/PercentageBar.kt
@@ -3,6 +3,8 @@ package com.fanhl.percentagebar
 import android.content.Context
 import android.graphics.Canvas
 import android.graphics.Color
+import android.graphics.drawable.Drawable
+import android.support.v4.content.ContextCompat
 import android.util.AttributeSet
 import android.view.View
 
@@ -16,8 +18,47 @@ class PercentageBar @JvmOverloads constructor(
     attrs: AttributeSet? = null,
     defStyleAttr: Int = 0
 ) : View(context, attrs, defStyleAttr) {
+
+    private var foregroundDrawable: Drawable? = null
+
+    var percent: Float = 0f
+
+    init {
+
+        val a = context.obtainStyledAttributes(attrs, R.styleable.PercentageBar, defStyleAttr, R.style.Widget_Percentage_Bar)
+
+        foregroundDrawable = a.getDrawable(R.styleable.PercentageBar_foregroundDrawable) ?: ContextCompat.getDrawable(context, R.drawable.img_percentage_bar_foreground_default)
+
+        percent = .5f // FIXME: 2018/9/30 fanhl
+
+        a.recycle()
+    }
+
     override fun onDraw(canvas: Canvas?) {
         super.onDraw(canvas)
-        canvas?.drawColor(Color.RED)
+
+        val validWidth = width - paddingLeft - paddingRight
+        val validHeight = height - paddingTop - paddingBottom
+
+        val saveCount = canvas?.save() ?: return
+        canvas.translate(paddingLeft.toFloat(), paddingRight.toFloat())
+
+        drawValid(canvas, validWidth, validHeight)
+
+        canvas.restoreToCount(saveCount)
+
+    }
+
+    private fun drawValid(canvas: Canvas, validWidth: Int, validHeight: Int) {
+        canvas.drawColor(Color.BLACK)
+        foregroundDrawable?.apply {
+            setBounds(
+                0,
+                0,
+                (validWidth * percent).toInt(),
+                validHeight
+            )
+            draw(canvas)
+        }
     }
 }
