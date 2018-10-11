e028762291f3fa37e0725683728a3005e14bb5db
fanhl <fanhonglin@klicen.com>
2018-10-11 10:33:00 +0800

    perc_test ok

--- library/src/test/java/com/fanhl/percentagebar/NumberUtilTest.kt
+++ library/src/test/java/com/fanhl/percentagebar/NumberUtilTest.kt
@@ -1,13 +1,25 @@
 package com.fanhl.percentagebar
 
-import org.junit.Test
-
 import org.junit.Assert.assertEquals
+import org.junit.Test
 
 class NumberUtilTest {
     @Test
     fun perc_test() {
-        //        NumberUtil.INSTANCE.perc()
-        assertEquals(4, (2 + 2).toLong())
+        mapOf(
+            1f to "100%",
+            2f to "200%",
+            .5f to "50%",
+            .55f to "55%",
+            .555f to "56%",
+            .554f to "55%",
+            -1f to "-100%",
+            -.5f to "-50%",
+            -.55f to "-55%",
+            -.555f to "-56%",
+            0f to "0%"
+        ).forEach { t, u ->
+            assertEquals(NumberUtil.perc(t), u)
+        }
     }
 }
