828658b4b96d1de5631e30f4c4619e514dd4d876
fanhl <fanhonglin@klicen.com>
2018-10-11 10:28:55 +0800

    NumberUtilTest kt

---
+++ library/src/test/java/com/fanhl/percentagebar/NumberUtilTest.kt
@@ -0,0 +1,13 @@
+package com.fanhl.percentagebar
+
+import org.junit.Test
+
+import org.junit.Assert.assertEquals
+
+class NumberUtilTest {
+    @Test
+    fun perc_test() {
+        //        NumberUtil.INSTANCE.perc()
+        assertEquals(4, (2 + 2).toLong())
+    }
+}
