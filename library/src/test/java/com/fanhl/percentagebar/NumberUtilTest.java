8a041c6e375765bc088f9118ade679ebeddda11f
fanhl <fanhonglin@klicen.com>
2018-10-11 10:28:43 +0800

    NumberUtilTest

---
+++ library/src/test/java/com/fanhl/percentagebar/NumberUtilTest.java
@@ -0,0 +1,13 @@
+package com.fanhl.percentagebar;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class NumberUtilTest {
+    @Test
+    public void  perc_test() {
+//        NumberUtil.INSTANCE.perc()
+        assertEquals(4, 2 + 2);
+    }
+}
